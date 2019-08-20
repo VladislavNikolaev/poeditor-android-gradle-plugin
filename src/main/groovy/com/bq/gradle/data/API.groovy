de68d4ed0dbd45e7e39fc2749faab8541f26d6c3
Paul Hostev <paul.hostev@gmail.com>
2019-08-20 17:40:59 +0300

    refactoring - API & Model

---
+++ src/main/groovy/com/bq/gradle/data/API.groovy
@@ -0,0 +1,107 @@
+package com.bq.gradle.data
+
+import groovy.json.JsonSlurper
+
+class API {
+
+    final static String POEDITOR_API_V2_UPLOAD_URL = 'https://api.poeditor.com/v2/projects/upload'
+    final static String POEDITOR_API_URL = 'https://poeditor.com/api/'
+
+    def model
+
+    API(ExtentionModel model) {
+        this.model = model
+    }
+
+    def list_languages(Callback callback) {
+        Https.post(
+                ['curl', '-X', 'POST',
+                 '-d', "api_token=${model.apiToken}",
+                 '-d', 'action=list_languages',
+                 '-d', "id=${model.projectId}",
+                 POEDITOR_API_URL],
+                callback
+        )
+    }
+
+    def translation_file_info(language, Callback callback) {
+        Https.post(
+                ['curl', '-X', 'POST',
+                 '-d', "api_token=${model.apiToken}",
+                 '-d', 'action=export',
+                 '-d', "id=${model.projectId}",
+                 '-d', 'type=android_strings',
+                 '-d', "language=${language}",
+                 POEDITOR_API_URL],
+                callback
+        )
+    }
+
+    def download_translation_file(String url, Callback callback) {
+        Https.download(url, callback)
+    }
+
+}
+
+class Https {
+
+    def _json
+    def _process
+    def _response
+    def _json_slurper
+
+    private Https(process) {
+        _json_slurper = new JsonSlurper()
+        _process = process
+    }
+
+    static def post(process, callback) {
+        def http = new Https(process)
+        def exception = http.check()
+        if (exception == null) callback.onSuccess(http.json())
+        else callback.onError(http.check())
+    }
+
+    static def download(url, callback) {
+        def http = new Https(['curl', '-X', 'GET', url])
+        def exception = http.check()
+        if (exception == null) callback.onSuccess(http.response())
+        else callback.onError(http.check())
+    }
+
+    private def response(process = _process) {
+        if (_response == null) _response = process.execute().text
+        _response
+    }
+
+    private def json(text = _response) {
+        if (_json == null) _json = json_slurper.parseText(text)
+        _json
+    }
+
+    private def check(response = response()) {
+        def code = response.response.code
+        if (code != "200") {
+            return new HttpsException(
+                    code,
+                    "An error occurred while trying to export from PoEditor API: \n\n" + json().toString()
+            )
+        }
+        null
+    }
+}
+
+interface Callback {
+    def onSuccess(json, exception)
+
+    def onError(exception)
+}
+
+class HttpsException extends IllegalStateException {
+    def code
+
+    HttpsException(code, message) {
+        super(message)
+        this.code = code
+    }
+}
---
+++ src/main/groovy/com/bq/gradle/data/ExtentionModel.groovy
@@ -0,0 +1,110 @@
+package com.bq.gradle.data
+
+class ExtentionModel {
+    def apiToken = ""
+    def projectId = ""
+    def defaultLang = ""
+    def resDirPath = ""
+    boolean generateTabletRes = false
+
+    def excludedLanguageCodes
+    def shouldReplaceBrandName = true
+
+    def keysExcludedForReplacement
+
+    def brandNameOld
+    def brandNameNew
+
+    private ExtentionModel(project, apiToken, projectId, defaultLang, resDirPath, generateTabletRes) {
+        this.apiToken = apiToken
+        this.projectId = projectId
+        this.defaultLang = defaultLang
+        this.resDirPath = resDirPath
+        this.generateTabletRes = generateTabletRes
+        this.excludedLanguageCodes = project.poEditorPlugin.excluded_language_codes.split(' ')
+        this.shouldReplaceBrandName = project.poEditorPlugin.replace_brand_name
+        this.keysExcludedForReplacement = project.extensions.poEditorPlugin.keys_excluded_for_replacement.split(" ")
+        this.brandNameOld = project.extensions.poEditorPlugin.brand_name_old
+        this.brandNameNew = project.extensions.poEditorPlugin.brand_name_new
+    }
+
+    static Builder builder() throws Exception {
+        new Builder()
+    }
+
+    static ExtentionModel define(project) {
+        def extantions = project.extantions
+        try {
+            builder().project(project)
+                    .apiToken(extantions.poEditorPlugin.api_token)
+                    .projectId(extantions.poEditorPlugin.project_id)
+                    .defaultLang(extantions.poEditorPlugin.default_lang)
+                    .resDirPath(extantions.poEditorPlugin.res_dir_path)
+                    .generateTabletRes(extantions.poEditorPlugin.generate_tablet_res)
+                    .build()
+        } catch (Exception e) {
+            throw new IllegalStateException(
+                    "You shoud define in your build.gradle: \n\n" +
+                            "poEditorPlugin.api_token = <your_api_token>\n" +
+                            "poEditorPlugin.project_id = <your_project_id>\n" +
+                            "poEditorPlugin.default_lang = <your_default_lang> \n" +
+                            "poEditorPlugin.res_dir_path = <your_res_dir_path> \n\n "
+                            + e.getMessage()
+            )
+        }
+    }
+}
+
+class Builder {
+    private def _project
+    private def _apiToken = ""
+    private def _projectId = ""
+    private def _defaultLang = ""
+    private def _resDirPath = ""
+    private def _generateTabletRes = false
+
+    def project(data) {
+        _project = data
+        this
+    }
+
+    def apiToken(data) {
+        if (data.length() == 0)
+            throw new Exception('Invalid params: api_token is ""')
+        _apiToken = data
+        this
+    }
+
+    def projectId(data) {
+        if (data.length() == 0)
+            throw new Exception('Invalid params: project_id is ""')
+        _projectId = data
+        this
+    }
+
+    def defaultLang(data) {
+        if (data.length() == 0)
+            throw new Exception('Invalid params: default_lang is ""')
+        _defaultLang = data
+        this
+    }
+
+    def resDirPath(data) {
+        if (data.length() == 0)
+            throw new Exception('Invalid params: res_dir_path is ""')
+        _resDirPath = data
+        this
+    }
+
+    def generateTabletRes(data) {
+        _generateTabletRes = data
+        this
+    }
+
+    ExtentionModel build() {
+        new ExtentionModel(
+                _apiToken, _projectId, _defaultLang, _resDirPath, _generateTabletRes
+        )
+    }
+}
+
